728fa427d01dd6769eab12182105380fb6f7d2b6
Prakapniou <prakapniou@gmail.com>
2023-06-23 21:42:12 +0300

    Add mapping

---
+++ EventsManager.Application/Mapping/MapProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace EventsManager.Application.Mapping;
+
+public sealed class MapProfile:Profile
+{
+    public MapProfile()
+    {
+        CreateMap<Event, EventDTO>().ReverseMap();
+        CreateMap<Speaker, SpeakerDTO>().ReverseMap();
+        CreateMap<Sponsor, SponsorDTO>().ReverseMap();
+    }
+}
--- EventsManager.Application/refs.cs
+++ EventsManager.Application/refs.cs
@@ -1,1 +1,6 @@
 ﻿global using EventsManager.Application.Main;
+global using AutoMapper;
+global using EventsManager.Application.DTOs;
+global using EventsManager.Domain.Aggregates.EventAggregate;
+global using EventsManager.Domain.Aggregates.SpeakerAggregate;
+global using EventsManager.Domain.Aggregates.SponsorAggregate;
