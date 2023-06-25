70e512f33f9dffde2c391e33a71a0f17c434be6a
Prakapniou <prakapniou@gmail.com>
2023-06-25 11:46:41 +0300

    Add generic service interface

---
+++ EventsManager.Application/Interfaces/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿namespace EventsManager.Application.Interfaces;
+
+internal interface IGenericService<TDTO> where TDTO : IBaseDTO
+{
+    public Task<IEnumerable<TDTO>> GetAsync();
+
+    public Task<TDTO> GetByIdAsync(Guid id);
+
+    public Task<TDTO> CreateAsync(TDTO dto);
+
+    public Task<TDTO> UpdateAsync(Guid id, TDTO dto);
+
+    public Task<bool> DeleteAsync(Guid id);
+}
